3d1233eec1055f4865c36b9a88c5cf2e7ea47909
Devaansh Mann <devaanshmann5@gmail.com>
2024-06-21 01:20:15 -0700

    added more

---
+++ arithmeticOperators.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(){
+
+
+    /*
+
+    order of operation:
+
+    parentesis
+    multiplication or division
+    addition or subtraction
+
+    */
+
+    int students = 20;
+
+
+    // Addition
+
+    // students = students + 1;
+    // students += 1;
+    // students++;        //(only for increment of 1)
+
+    // Subtraction
+    
+    // students = students - 1;
+    // students -= 1;
+    // students--;        //(only for a decrement of 1)
+
+    // Multiplication
+    
+    // students = students * 2;
+    // students *= 2;
+
+    // Division
+    
+    // students = students / 2;
+    // students /= 2;
+
+    // Modulus - finds the remainder
+    int remainder = students % 3;
+
+    // std::cout<< students;
+    std::cout<< remainder;
+
+    return 0;
+}
---
+++ typedef.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+
+the whole long identification of the typedef can be put into the identifier name at the end. the identifier name ends with a '_t'.
+pairlist_t is the new identifier
+
+*/
+
+// OLD WAY
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+
+//  NEW WAY
+
+// using pairlist_t = std::vector<std::pair<std::string, int>>;
+using text_t = std::string;
+using number_t = int;
+
+int main(){
+
+    // pairlist_t pairlist;
+
+    text_t firstName = "Devaansh";
+    text_t lastName = "Mann";
+    number_t age = 21;
+
+
+    std::cout << firstName << '\n';
+    std::cout << lastName << '\n';
+    std::cout << age << '\n';
+
+    return 0;
+}
