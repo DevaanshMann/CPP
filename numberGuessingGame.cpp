d2894d22c84400092ec2bd3cbde4361158ea7534
Devaansh Mann <devaanshmann5@gmail.com>
2025-01-22 10:34:36 -0800

    Learning C++

---
+++ numberGuessingGame.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+    int num;
+    int tries;
+    int guess;
+
+    time_t current_time = time(nullptr);
+
+    num = (rand() % 100) + 1;
+
+    cout << "*******NUMBER GUESSING GAME********\n";
+
+    do
+    {
+        cout << "Enter a guess between (1-100): ";
+        cin >> guess;
+        tries++;
+
+
+        if (guess > num)
+        {
+            cout << "Too HIGH\n";
+        }
+        
+        else if (guess < num)
+        {
+            cout << "Too LOW\n";
+        }
+
+        else{
+            cout << "CORRECT!\n" << "Number of tries: " << tries;
+        }
+
+    } while (guess != num);
+    
+    return 0;
+}
